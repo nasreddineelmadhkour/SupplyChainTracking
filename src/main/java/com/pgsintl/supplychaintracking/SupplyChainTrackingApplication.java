2ab878b47f9b79eea44b173cf4d3f2ad2da5c147
Nasreddine El Madhkour <90349923+nasreddineelmadhkour@users.noreply.github.com>
2024-06-28 16:00:55 +0100

    Update SupplyChainTrackingApplication.java

--- src/main/java/com/pgsintl/supplychaintracking/SupplyChainTrackingApplication.java
+++ src/main/java/com/pgsintl/supplychaintracking/SupplyChainTrackingApplication.java
@@ -11,21 +11,13 @@ public class SupplyChainTrackingApplication implements CommandLineRunner {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-
     public static void main(String[] args) {
         SpringApplication.run(SupplyChainTrackingApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        createDatabaseIfNotExists();
+
     }
 
-    private void createDatabaseIfNotExists() {
-        // Example SQL to create database if it doesn't exist
-        jdbcTemplate.execute("IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = 'pgsDB')\n" +
-                "BEGIN\n" +
-                "    CREATE DATABASE pgsDB;\n" +
-                "END;");
-    }
 }
