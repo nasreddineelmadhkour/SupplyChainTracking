32b4a391850a1733ec23bc1b218c7a82d8100743
Nasreddine El Madhkour <90349923+nasreddineelmadhkour@users.noreply.github.com>
2024-07-04 16:01:21 +0100

    Update SupplyChainTrackingApplication.java

--- src/main/java/com/pgsintl/supplychaintracking/SupplyChainTrackingApplication.java
+++ src/main/java/com/pgsintl/supplychaintracking/SupplyChainTrackingApplication.java
@@ -16,10 +16,7 @@ import java.util.Date;
 @SpringBootApplication
 public class SupplyChainTrackingApplication implements CommandLineRunner {
 
-    @Autowired
-    AccountRepository accountRepository;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
     public static void main(String[] args) {
@@ -28,21 +25,6 @@ public class SupplyChainTrackingApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
-
-        Account account = new Account();
-        account= accountRepository.findById(1L).orElse(null);
-
-        if(account==null){
-            Account account2 = new Account();
-            account2.setPassword(passwordEncoder.encode("admin"));
-            account2.setName("Nasreddine Madhkour");
-            account2.setEmail("nasreddine.elmadhkour@gmail.com");
-            account2.setRole(Role.CARRIER);
-            account2.setPhoneNumber("28000046");
-            account2.setDatecreation(new Date());
-            accountRepository.save(account2);
-        }
 
 
     }
