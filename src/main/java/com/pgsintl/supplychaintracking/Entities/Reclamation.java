6143d3edb6aef059d0ab18a3a6a557ee16609b3a
nasreddine <90349923+nasreddineelmadhkour@users.noreply.github.com>
2024-07-10 15:52:12 +0100

    update

--- src/main/java/com/pgsintl/supplychaintracking/Entities/Reclamation.java
+++ src/main/java/com/pgsintl/supplychaintracking/Entities/Reclamation.java
@@ -8,15 +8,18 @@ import java.util.Date;
 
 @Entity
 @Table(name = "reclamation")
+@Builder
 @Setter
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class Reclamation implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long reclamationNumber;
     String description;
-    Date dateReclamation;
+    Date dateReclamation = new Date();
     @Enumerated(EnumType.STRING)
     StatusReclamation statusReclamation;
+
 }
