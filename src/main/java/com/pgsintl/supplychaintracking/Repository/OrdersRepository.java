9224175d40ce4aad60d9b21ca13519ddccf9f0cb
nasreddine <90349923+nasreddineelmadhkour@users.noreply.github.com>
2024-08-01 02:45:28 +0100

    backup

--- src/main/java/com/pgsintl/supplychaintracking/Repository/OrdersRepository.java
+++ src/main/java/com/pgsintl/supplychaintracking/Repository/OrdersRepository.java
@@ -1,18 +1,7 @@
 package com.pgsintl.supplychaintracking.Repository;
 
 import com.pgsintl.supplychaintracking.Entities.Orders;
-import com.pgsintl.supplychaintracking.Entities.StatusOrders;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
-import java.util.List;
-
 public interface OrdersRepository extends JpaRepository<Orders,Long> {
-
-    List<Orders> findByReclamationIsNotNullAndCarrierUserNumber(Long idCarrier);
-
-    List<Orders> findByReclamationIsNotNullAndDriver_UserNumber(Long idDriver);
-
-    List<Orders> findByDateOrdersIsBetweenAndStatus(Date dateAfter,Date dateBefore, StatusOrders statusOrders);
-
 }
